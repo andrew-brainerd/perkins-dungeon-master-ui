e2646a363819cb3de31a615eaabdec3ad87ce307
Andrew Brainerd <drwb333@gmail.com>
2020-03-08 13:51:05 -0400

    Add main menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../common/Button/Button';
 import styles from './Menu.module.scss';
 
 const Menu = () => {
+  const [showInputs, setShowInputs] = useState(true);
+
   return (
-    <div className={styles.menu}>
-      Welcome to Perkins Dungeon Master
+    <div className={styles.landing}>
+      <h1>Welcome to Perkins Dungeon Master</h1>
+      <div className={styles.menu}>
+        <Button
+          text={'Sign Up'}
+        />
+        <Button
+          text={'Sign In'}
+          onClick={() => setShowInputs(true)}
+        />
+        {showInputs &&
+          <div className={styles.inputContainer}>
+          </div>
+        }
+      </div>
     </div>
   );
 };
