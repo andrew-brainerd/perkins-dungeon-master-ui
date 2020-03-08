f669ee1161f85cd53f65fba383ab7e22e75779fe
Andrew Brainerd <drwb333@gmail.com>
2020-03-08 15:46:34 -0400

    Add Auth0

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,25 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useAuth0 } from '../../hooks/useAuth0';
 import Button from '../common/Button/Button';
 import styles from './Menu.module.scss';
 
 const Menu = () => {
-  const [showInputs, setShowInputs] = useState(true);
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
   return (
     <div className={styles.landing}>
-      <h1>Welcome to Perkins Dungeon Master</h1>
+      <h1>Perkins Dungeon Master</h1>
       <div className={styles.menu}>
-        <Button
-          text={'Sign Up'}
-        />
-        <Button
-          text={'Sign In'}
-          onClick={() => setShowInputs(true)}
-        />
-        {showInputs &&
-          <div className={styles.inputContainer}>
-          </div>
-        }
+        {!isAuthenticated && (
+          <Button
+            text={'Sign In'}
+            onClick={() => loginWithRedirect()}
+          />
+        )}
+        {isAuthenticated && (
+          <Button
+            text={'Sign Out'}
+            onClick={() => logout()}
+          />
+        )}
       </div>
     </div>
   );
