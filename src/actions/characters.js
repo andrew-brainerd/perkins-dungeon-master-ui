c2dff0b15ec6f0a62ad412c38cd953dcdee81e7f
Andrew Brainerd <drwb333@gmail.com>
2020-03-24 12:38:04 -0400

    Move error handling to action layer

--- src/actions/characters.js
+++ src/actions/characters.js
@@ -10,35 +10,37 @@ export const CREATING_CHARACTER = `${PREFIX}/CREATING_CHARACTER`;
 export const CREATING_CHARACTER_FAILED = `${PREFIX}/LOADING_CHARACTER_FAILED`;
 export const LOADING_CHARACTER = `${PREFIX}/LOADING_CHARACTER`;
 export const CHARACTER_LOADED = `${PREFIX}/CHARACTER_CREATED`;
+export const LOADING_CHARACTER_FAILED = `${PREFIX}/LOADING_CHARACTER_FAILED`;
 
 export const creatingCharacter = { type: CREATING_CHARACTER };
 export const creatingCharacterFailed = err => ({ type: CREATING_CHARACTER_FAILED, err });
 export const loadingCharacter = { type: LOADING_CHARACTER };
 export const characterLoaded = character => ({ type: CHARACTER_LOADED, character });
+export const loadingCharacterFailed = err => ({ type: LOADING_CHARACTER_FAILED, err });
 
 export const createCharacter = character => async (dispatch, getState) => {
-  const playerId = getCurrentPlayerId(getState());
-
-  const newCharacter = { ...character, playerId };
-
   dispatch(creatingCharacter);
-  charactersApi.createCharacter(newCharacter).then(newCharacter => {
-    console.log(newCharacter);
-    if (newCharacter.message && newCharacter.message.toLowerCase().includes('error')) {
-      dispatch(creatingCharacterFailed(newCharacter.message));
-    } else {
-      dispatch(characterLoaded(newCharacter));
-      dispatch(navTo(GAME_ROUTE.replace(':gameId', character.gameId)));
-    }
-  });
+
+  try {
+    const playerId = getCurrentPlayerId(getState());
+    const newCharacter = { ...character, playerId };
+    const createdCharacter = await charactersApi.createCharacter(newCharacter);
+
+    dispatch(characterLoaded(createdCharacter));
+    dispatch(navTo(GAME_ROUTE.replace(':gameId', character.gameId)));
+  } catch (err) {
+    dispatch(creatingCharacterFailed(err));
+  }
 };
 
 export const loadCharacter = characterId => async (dispatch, getState) => {
   const gameId = getCurrentGameId(getState());
 
   dispatch(loadingCharacter);
-  charactersApi.loadCharacter(gameId, characterId).then(character => {
-    dispatch(characterLoaded(character));
-    dispatch(navTo(GAME_ROUTE.replace(':gameId', gameId)));
-  });
+  charactersApi.loadCharacter(gameId, characterId)
+    .then(character => {
+      dispatch(characterLoaded(character));
+      dispatch(navTo(GAME_ROUTE.replace(':gameId', gameId)));
+    })
+    .catch(err => dispatch(loadingCharacterFailed(err)));
 };
--- src/api/characters.js
+++ src/api/characters.js
@@ -2,17 +2,13 @@ import { prop } from 'ramda';
 import { client } from './tools';
 
 export const createCharacter = async character => {
-  const response = await client.post('/characters', { ...character })
-    .then(prop('data'))
-    .catch(err => console.error(err));
+  const response = await client.post('/characters', { ...character });
 
-  return response;
+  return prop('data', response);
 };
 
 export const loadCharacter = async characterId => {
-  const response = await client.get('/characters', { params: { characterId } })
-    .then(prop('data'))
-    .catch(err => console.error(err));
+  const response = await client.get('/characters', { params: { characterId } });
 
-  return response;
+  return prop('data', response);
 };
