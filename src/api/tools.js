d8ae7aeb649142908f6efa5da095a82835958af7
Andrew Brainerd <drwb333@gmail.com>
2020-04-01 14:19:42 -0400

    Update app name

--- src/api/tools.js
+++ src/api/tools.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const basicJsonHeader = { 'Content-Type': 'application/json' };
 
 export const client = axios.create({
-  baseURL: process.env.REACT_APP_PERKINS_API_URL || 'http://localhost:5000/api/',
+  baseURL: process.env.REACT_APP_ANORAK_API_URL || 'http://localhost:5000/api/',
   headers: basicJsonHeader
 });
