af709607edb206ae312be2dd5de679efa6f6174c
Andrew Brainerd <drwb333@gmail.com>
2020-04-01 00:05:22 -0400

    Update unit test

--- src/components/Game/Game.test.js
+++ src/components/Game/Game.test.js
@@ -2,20 +2,26 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
-import { history } from '../../store/configureStore';
 import { Provider } from 'react-redux';
 import Game from './Game';
 
 describe('Game Component', () => {
-  let store;
+  let store, props;
 
   beforeEach(() => {
     store = configureStore([thunk])();
+
+    props = {
+      height: 500,
+      connectClient: jest.fn(),
+      loadGame: jest.fn(),
+      loadCharacters: jest.fn()
+    };
   });
 
   const render = () => shallow(
     <Provider store={store}>
-      <Game history={history} />
+      <Game {...props} />
     </Provider>
   );
 
