8a7c01b07f3ed31eb311a48233e25895b0c34c29
Andrew Brainerd <drwb333@gmail.com>
2020-04-03 14:50:00 -0400

    Move HTML to constants

--- src/api/game.js
+++ src/api/game.js
@@ -1,6 +1,7 @@
 import { prop } from 'ramda';
 import { client } from './tools';
 import { parseServerResponse } from '../utils/game';
+import { inviteBodyStyle, headerStyle, inviteButtonStyle, getInviteHtml } from '../constants/game';
 
 export const createGame = async (name, createdBy = 'System') => {
   const response = await client.post('/games', { name, createdBy })
@@ -36,59 +37,13 @@ export const processPlayerInput = async (gameId, input) => {
   return parseServerResponse(input);
 };
 
-const inviteBodyStyle = `
-  background-color: black !important;
-  height: 300px;
-  width: 550px;
-`.replace(/\n/g, '');
-
-const headerStyle = `
-  color: #00fcfe;
-  margin: 30px auto;
-  padding: 30px;
-  text-align: center;
-`.replace(/\n/g, '');
-
-const inviteButtonStyle = `
-  background-color: #fffd33;
-  border-radius: 5px;
-  margin: 30px auto;
-  padding: 10px 15px;
-  text-align: center;
-  width: 150px;
-`.replace(/\n/g, '');
-
 export const sendInvite = async (gameId, playerName, email) => {
-  const { protocol, hostname } = process.env.NODE_ENV === 'production' ?
-    window.location : { protocol: 'http:', hostname: 'localhost:3000' };
-
   const response = await client.post('/messaging', {
     gameId,
     to: email,
     from: 'noreply@anorakgm.com',
     subject: `${playerName} invites you on an adventure!`,
-    html: `
-      <html>
-        <head>
-          <style type="text/css">
-            a, a:link, a:visited {
-              color: black !important;
-              font-size: 22px;
-              font-weight: bold;
-              text-decoration: none;
-            }
-          </style>
-        </head>
-        <body style="${inviteBodyStyle}">
-          <h2 style="${headerStyle}">You're invited on an adventure!</h2>
-          <div style="${inviteButtonStyle}">
-            <a href="${protocol}//${hostname}/game/${gameId}/setup">
-              Join Game
-            </a>
-          </div>
-        </body>
-      </html>
-    `.replace(/\n/g, '')
+    html: getInviteHtml(gameId, window.location)
   });
 
   return prop('data', response);
--- src/constants/game.js
+++ src/constants/game.js
@@ -1,3 +1,53 @@
 export const localCommands = [
   'help'
 ];
+
+export const inviteBodyStyle = `
+  background-color: black !important;
+  height: 300px;
+  width: 550px;
+`.replace(/\n/g, '');
+
+export const headerStyle = `
+  color: #00fcfe;
+  margin: 30px auto;
+  padding: 30px;
+  text-align: center;
+`.replace(/\n/g, '');
+
+export const inviteButtonStyle = `
+  background-color: #fffd33;
+  border-radius: 5px;
+  margin: 30px auto;
+  padding: 10px 15px;
+  text-align: center;
+  width: 150px;
+`.replace(/\n/g, '');
+
+export const getInviteHtml = (gameId, location) => {
+  const { protocol, hostname } = process.env.NODE_ENV === 'production' ?
+    location : { protocol: 'http:', hostname: 'localhost:3000' };
+
+  return `
+    <html>
+      <head>
+        <style type="text/css">
+          a, a:link, a:visited {
+            color: black !important;
+            font-size: 22px;
+            font-weight: bold;
+            text-decoration: none;
+          }
+        </style>
+      </head>
+      <body style="${inviteBodyStyle}">
+        <h2 style="${headerStyle}">You're invited on an adventure!</h2>
+        <div style="${inviteButtonStyle}">
+          <a href="${protocol}//${hostname}/game/${gameId}/setup">
+            Join Game
+          </a>
+        </div>
+      </body>
+    </html>
+  `.replace(/\n/g, '');
+};
